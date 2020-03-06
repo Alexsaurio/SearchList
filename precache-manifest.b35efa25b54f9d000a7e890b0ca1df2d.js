acd3ab132d0beb5df55aae067e649144fbf18a04
Alejandro AR <alex-ar@outlook.com>
2020-03-06 12:53:25 -0600

    Updates

---
+++ precache-manifest.b35efa25b54f9d000a7e890b0ca1df2d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "794f78227ccca95e87b3",
+    "url": "/SearchList/static/js/runtime~main.7bdfd098.js"
+  },
+  {
+    "revision": "10b50bd296939918f992",
+    "url": "/SearchList/static/js/main.8727ea33.chunk.js"
+  },
+  {
+    "revision": "beb593ec0c56929b246c",
+    "url": "/SearchList/static/js/2.30e40ef2.chunk.js"
+  },
+  {
+    "revision": "10b50bd296939918f992",
+    "url": "/SearchList/static/css/main.1fd5f7d3.chunk.css"
+  },
+  {
+    "revision": "e4114968ad3c6a60eca616cb8fa2c42c",
+    "url": "/SearchList/index.html"
+  }
+];
